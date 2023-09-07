41384642e76fd573736e314526073a3fe1ca226e
hoquocnhat1506 <89431816+hoquocnhat1506@users.noreply.github.com>
2023-09-07 11:12:09 +0700

    first commit

---
+++ src/components/password/index.jsx
@@ -0,0 +1,77 @@
+import style from "./style.module.css"; 
+
+function Password() {
+    const[password]
+  return (
+    <form>
+      <div className={style.title}>PASSWORD GENERATOR</div>
+      <div className={style.text}>
+        Create strong and secure passwords to keep your account safe online.
+      </div>
+      <div className={style.boxinput}>
+        <input type="text" />
+        <svg
+          className={style.icoin1}
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline points="23 4 23 10 17 10"></polyline>
+          <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path>
+        </svg>
+        <button>
+          <svg
+            stroke="currentColor"
+            fill="none"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="button-icon"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6 4a2 2 0 0 1 2-2h10l4 4v10.2a2 2 0 0 1-2 1.8H8a2 2 0 0 1-2-2Z"></path>
+            <path d="M10 2v4h6"></path>
+            <path d="M18 18v-7h-8v7"></path>
+            <path d="M18 22H4a2 2 0 0 1-2-2V6"></path>
+          </svg>
+          <div className={style.textbutton}>Copy</div>
+        </button>
+      </div>
+      <div className={style.length}>Too short</div>
+      <div className={style.password}>
+        <div className={style.passwordlength}>Password Length:</div>
+        <input type="range" />
+      </div>
+      <div className={style.setting}>
+        <ul>
+          <li className={style.choose}>
+            <label>Uppercase</label>
+            <input type="checkbox" />
+          </li>
+          <li className={style.choose}>
+            <label>Lowercase</label>
+            <input type="checkbox" />
+          </li>
+          <li className={style.choose}>
+            <label>Numbers</label>
+            <input type="checkbox" />
+          </li>
+          <li className={style.choose}>
+            <label>Special Characters</label>
+            <input type="checkbox" />
+          </li>
+        </ul>
+      </div>
+    </form>
+  );
+}
+export default Password;
